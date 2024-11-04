1b76fd817937c7f2f15e0665ba195676bd8b0ae9
Aleksander <ShuSashi@yandex.ru>
2024-11-04 04:47:51 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -25,56 +25,6 @@ function my_scripts_method() {
 	
 }
 
-/* Дополнительные сущности (кастомные поля) */
-
-// Регистрируеме новую тип для слайдера.
-function register_events() {
-	// Для Типа записи Слайдер
-	register_post_type( 'sliders', [
-		'labels'	=> [
-			'name'					=> 'Hero Slider',
-			'singular_name'			=> 'Hero Sliders',
-			'name_admin_bar'		=> 'Hero Sliders',
-			'menu_name'				=> 'Hero Sliders',
-			'add_new'				=> 'Add Hero Slider',
-			'add_new_item'			=> 'Add Hero Slider',
-			'edit_item'				=> 'Edit Hero Slider',
-			'new_item'				=> 'New Hero Slider',
-			'view_item'				=> 'View Hero Slider',
-			'search_items'			=> 'Search Hero Slider',
-			'not_found'				=> 'Hero Slider Not Found',
-			'not_found_in_trash'	=> 'Hero Slider Not Found In Trash',
-			'featured_image'		=> 'Hero Slider Photo',
-			'set_featured_image'	=> 'Set Hero Slider Photo',
-			'title_placeholder'		=> 'Hero Slider Title',
-			'first_menu_name'		=> 'Hero Slider List',
-		],
-		'public'				=> true,
-		'publicly_queryable'	=> true,
-		'rewrite'				=> array( 'slug' => 'sliders', 'hierarchical' => false, 'with_front' => false ),
-		'capability_type'		=> 'post',
-		'has_archive'			=> true,
-		'hierarchical'			=> false,
-		'supports'				=> array( 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields', 'page-attributes', 'post-formats' ),
-		'menu_position'			=> 7,
-		'menu_icon'				=> 'dashicons-slides',
-		'posts_per_page'		=> 17,
-		'show_in_rest'			=> true,
-		'show_in_nav_menus'		=> true,
-		'show_ui'				=> true,
-		'show_in_menu'			=> true,
-	] );
-}
-add_action( 'init', 'register_events' );
-
-
-
-/* Дополнительные сущности (кастомные поля) */
-
-
-
-
-
 /* Ваш дополнительный код */
 if (! function_exists('sidra_setup')) {
 	function sidra_setup () {
@@ -98,9 +48,6 @@ function the_truncated_post($symbol_amount) {
 	$filtered = strip_tags( preg_replace('@<style[^>]*?>.*?</style>@si', '', preg_replace('@<script[^>]*?>.*?</script>@si', '', apply_filters('the_content', get_the_content()))) );
 	echo substr($filtered, 0, strrpos(substr($filtered, 0, $symbol_amount), ' ')) . '...';
 }
-
-
-
 
 // Регистрируеме новую таксономию и тип поста.
 function register_kb() {
@@ -172,7 +119,7 @@ function kb_custom_column( $column_name, $post_id ) {
 }
 add_action( 'manage_knowledgebase_posts_custom_column', 'kb_custom_column', 10, 2 );
 
-//
+// функция для секции базы данных
 function kb_sections( $sections = array(), $active_section = null ) {
     $taxonomy = 'section';
     $link_class = '';
